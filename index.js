b4ed9f6ed2457fedeb09cea3ea3e3b5404b063b9
aminlimbu <aminlimbu@htmail.com>
2023-03-28 17:28:09 +1100

    path and ejs-mate added, views created with partials and boilerplate

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const path = require("path");
+const ejsMate = require("ejs-mate");
 
 const app = express();
 
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/", (req, res) => {
-    res.send("homepage");
+    res.render("landingPage/index");
 });
 
 app.listen(3000, (req, res) => {
