a1ffd6f997c535053d984cbd8e85cce948f73eec
aminlimbu <aminlimbu@htmail.com>
2023-04-05 23:21:46 +1000

    Navbar style updated, public folder added with express.static middleware, footer partial added, app.css added, boilerpalte updated'

--- index.js
+++ index.js
@@ -8,8 +8,23 @@ app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
     res.render("landingPage/index");
+});
+
+app.get("/amplifier", (req, res) => {
+    res.send(" Page under Construction");
+});
+app.get("/receiver", (req, res) => {
+    res.send("Page under Construction");
+});
+app.get("/network-player", (req, res) => {
+    res.send("Page under Construction");
+});
+app.get("/cd-player", (req, res) => {
+    res.send("Page under Construction");
 });
 
 app.listen(3000, (req, res) => {
