f6debe58b98118c3c8a8009fa67bf07ab6603e36
aminlimbu <aminlimbu@htmail.com>
2023-04-26 22:54:19 +1000

    product show page added, created new product, new product updated in mongoDB, app product list page added

--- Routes/products.js
+++ Routes/products.js
@@ -2,6 +2,10 @@ const express = require("express");
 const product = require("../controllers/products");
 const router = express.Router();
 
+router.route("/").get(product.renderAllProducts);
+
 router.route("/new").get(product.renderNewForm).post(product.newProduct);
+
+router.route("/show/:id").get(product.showProduct);
 
 module.exports = router;
--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,37 @@
+const Product = require("../models/Product");
+
 // renders form to add new item, validation and database connection pending ...
 module.exports.renderNewForm = (req, res) => {
     res.render("products/new");
 };
 
-// ...pending
-module.exports.newProduct = (req, res) => {
-    console.log(req.body);
-    res.send("Hello");
+module.exports.renderAllProducts = async (req, res) => {
+    const products = await Product.find();
+    res.render("products/all", { products });
+};
+
+// created new product
+module.exports.newProduct = async (req, res) => {
+    const product = new Product(req.body.product);
+
+    await product.save();
+
+    // redirect to new product
+    res.redirect(`/products/show/${product.id}`);
+};
+
+module.exports.showProduct = async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render("products/show", { product });
+};
+
+module.exports.updateProduct = async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, {
+        ...req.body.product,
+    });
+    await product.save();
+
+    res.redirect(`/product/show/${id}`);
 };
