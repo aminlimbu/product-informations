ffc1de91c9d2a6d8f370082c161323bc52fdf22f
aminlimbu <aminlimbu@htmail.com>
2023-09-18 17:26:39 +1000

    comments

--- middlewares/permissions.js
+++ middlewares/permissions.js
@@ -1,7 +1,7 @@
 // checks if the user is logged in
 // Middleware to authorise user actions
 module.exports.isLoggedIn = (req, res, next) => {
-    // isAuthienticate from possport middleware method
+    // isAuthienticate is possport middleware method
     if (!req.isAuthenticated()) {
         // set url to return to, after login
         req.session.redirectTo = req.originalUrl;
